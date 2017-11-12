33666ac9e61c4ebaa0fcb89c2d8a7cfe62d5e885
David Tyler <davet.code@gmail.com>
2017-11-12 09:51:44 +0000

    Add project files.

---
+++ SQLServerSchemaExporter.CommandLineApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+
+namespace SQLServerSchemaExporter.CommandLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.Command("extract", (command) =>
+            {
+                command.Description = "Extract a mock schema from the database specified";
+                command.HelpOption("-?|-h|--help");
+                var serverOption = command.Option("-s|--server", "The SQL Server where the database lives", CommandOptionType.SingleValue);
+                var dbOption = command.Option("-d|--db", "The name of the database on the server", CommandOptionType.SingleValue);
+            });
+        }
+    }
+}
